cf2aa1e629e1090d04ba2bcfef3e9f7c5addf503
“chie-hirabayashi” <nyantoroman@yahoo.co.jp>
2022-10-17 22:44:40 +0900

    カレンダーにEventモデルのデータ表示

--- app/Http/Controllers/FullCalendarController.php
+++ app/Http/Controllers/FullCalendarController.php
@@ -3,8 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Event;
 
 class FullCalendarController extends Controller
 {
-    //
+    public function index(Request $request)
+    {
+        $data = Event::where('user_id', $request->user()->id)
+                ->whereDate('start', '>=', $request->start)
+                ->whereDate('end', '<=', $request->end)
+                ->get(['id', 'title', 'start', 'end']);
+    
+        return response()->json($data);
+    }
 }
--- resources/js/calendar.js
+++ resources/js/calendar.js
@@ -23,33 +23,7 @@ const calendar = new Calendar(calendarEl, {
     nextDayThreshold: '00:00:00',
 
     // データを用意
-    events: [
-        {
-            'title': 'スパルタキャンプ',
-            'start': '2022-10-01 08:30',
-            'end': '2022-10-01 18:30'
-        },
-        {
-            'title': 'スパルタキャンプ',
-            'start': '2022-10-02 08:30',
-            'end': '2022-10-02 18:30'
-        },
-        {
-            'title': 'スパルタキャンプ',
-            'start': '2022-10-08 08:30',
-            'end': '2022-10-08 18:30'
-        },
-        {
-            'title': 'スパルタキャンプ',
-            'start': '2022-10-09 08:30',
-            'end': '2022-10-09 18:30'
-        },
-        {
-            'title': '山賊まつり',
-            'start': '2022-10-08',
-            'end': '2022-10-10'
-        }
-    ],
+    events: '/calendar/action',
 });
 
 calendar.render();
