e2590d6b5a822fdd21fe52a676001f573c95bec4
“chie-hirabayashi” <nyantoroman@yahoo.co.jp>
2022-10-17 22:12:28 +0900

    カレンダー表示 $ sail npm run hot

--- resources/js/calendar.js
+++ resources/js/calendar.js
@@ -1,7 +1,9 @@
 import { Calendar } from "@fullcalendar/core";
+import jaLocale from "@fullcalendar/core/locales/ja";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
 
+// htmlにidで埋め込み
 const calendarEl = document.getElementById("calendar");
 
 const calendar = new Calendar(calendarEl, {
@@ -10,8 +12,15 @@ const calendar = new Calendar(calendarEl, {
     headerToolbar: {
         left: "prev,next today",
         center: "title",
-        right: "dayGridMonth,dayGridWeek,dayGridDay,dayGrid,timeGridWeek,timeGridDay,timeGrid",
+        right: "dayGridMonth,timeGridWeek,timeGridDay",
+        // right: "dayGridMonth,dayGridWeek,dayGridDay,dayGrid,timeGridWeek,timeGridDay,timeGrid",
     },
+    // 日本語化
+    locales: jaLocale,
+    locale: 'ja',
+
+    // 日付表示の境界線時刻を設定(デフォルトは9時)
+    nextDayThreshold: '00:00:00',
 });
 
 calendar.render();
