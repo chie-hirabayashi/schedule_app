f70e131aaa18540bdffebda256fa5142397f5573
“chie-hirabayashi” <nyantoroman@yahoo.co.jp>
2022-10-18 14:52:11 +0900

    メール送信テスト

--- app/Console/Commands/SendMails.php
+++ app/Console/Commands/SendMails.php
@@ -3,6 +3,9 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\Schedule;
+use App\Models\User;
 
 class SendMails extends Command
 {
@@ -37,7 +40,17 @@ class SendMails extends Command
      */
     public function handle()
     {
-        // return 0;
         echo 'SendMail';
+
+        $user = User::first();
+        
+        // テキストメールで短文の場合
+        Mail::raw('本文です', function ($message) use ($user) {
+            $message->to($user->email)
+                ->subject('タイトルです');
+        });
+        // メール用クラスのMailableを利用する場合
+        Mail::to($user->email)
+            ->send(new Schedule());
     }
 }
